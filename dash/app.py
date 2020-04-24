1a68a2c7ab9bd5875e7a3b6010cfa2a49dfc92c5
herve <herve.mignot@equancy.com>
2020-04-24 17:23:16 +0200

    Adjust for deployment

--- dash/app.py
+++ dash/app.py
@@ -1,15 +1,25 @@
+import os
+
 import dash
 import dash_bootstrap_components as dbc
 import dash_auth
-import sys
+#import sys
+
+
 #Initializing our app & calling the external stylesheet
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = 'COVID-19 AMSE Project'
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-sys.path.append('../')
-from confidential.secrets import VALID_USERNAME_PASSWORD_PAIRS
+#sys.path.append('../')
+
+
+if os.environ.get('COVID_APP_ENV', 'local') != 'local':
+   from ..confidential.secrets import VALID_USERNAME_PASSWORD_PAIRS
+else:
+   from local_env import VALID_USERNAME_PASSWORD_PAIRS
+
 
 auth = dash_auth.BasicAuth(
     app,
