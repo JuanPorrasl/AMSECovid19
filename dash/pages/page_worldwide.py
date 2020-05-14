4abdc7157b3635f48466ec0c1b1f9e99918dae29
thomaspical <thomascannes@gmail.com>
2020-05-14 23:07:06 +0200

    Major design and ergonomy update on page_worldwide

--- dash/pages/page_worldwide.py
+++ dash/pages/page_worldwide.py
@@ -20,6 +20,36 @@ import plotly.express as px
 #Python Script
 from cleaning_datas import df, today, yesterday, last_file_hopkins, config
 
+
+color_confirmed="#fc5c65"
+color_recovered="#45aaf2"
+color_deaths="#4b6584"
+
+menu_ranges=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                         label="MTD",
+                         step="month",
+                         stepmode="todate"),
+                    dict(count=1,
+                         label="1m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=3,
+                         label="3m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(count=6,
+                         label="6m",
+                         step="month",
+                         stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            type="date"
+        )
+
 ############# PLOT PART #############
 #Dropdown for neighbourhood
 dropdown_options = [
@@ -37,81 +67,103 @@ body_worldwide = dbc.Container(
                                 html.H2(id="title-main"),
                                 html.P(["Latest Updates: ",dbc.Badge("Hopkins: "+str(last_file_hopkins), color="secondary", className="mr-1")])
                             ],
-                            md=9,
+                            md=10,
                         ),
                         dbc.Col(
                             [
-                                dcc.Dropdown(
-                                    id="selected_country",
-                                    options=dropdown_options,
-                                    value='Worldwide'                            
+                                dcc.DatePickerSingle(
+                                    id='map-slider',
+                                    min_date_allowed=df.Last_Update.min(),
+                                    max_date_allowed=df.Last_Update.max(),
+                                    initial_visible_month=df.Last_Update.max(),
+                                    date=df.Last_Update.max(),
+                                    display_format='D/M/Y',
                                 ),
                             ],
-                            md=3,
-                        )
+                        md=2,
+                        ),
                     ],
                     justify="center",
                 ),
                 fluid=False,
                 className="mb-2"
             ),
-            #Cards
+            #Worldmap
             dbc.Row(
                 [
-                    dbc.Col(
-                        [
-                        dbc.Card(
-                            dbc.CardBody([
-                                    html.P("Total Confirmed", className="card-title"),
-                                    html.H4(id="card-confirmed", className="card-text"),
-                            ]),
-                            #Total confirmed,
-                            inverse=True, className="mb-1",
-                            style={"text-align":"center", 'background-color': "#384259"}
-                        ),
-                        dbc.Card(
-                            dbc.CardBody([
-                                    html.P("Total Recovered", className="card-title"),
-                                    html.H4(id="card-recovered", className="card-text"),
-                            ]),
-                            #Total recovered
-                            inverse=True, className="mb-1",
-                            style={"text-align":"center", 'background-color': "#7ac7c4"}
-                        ),
-                        dbc.Card(
-                            dbc.CardBody([
-                                    html.P("Total Deaths", className="card-title"),
-                                    html.H4(id="card-deaths", className="card-text"),
-                            ]),
-                            #Total deaths
-                            inverse=True, className="mb-2",
-                            style={"text-align":"center", 'background-color': "#f73859"}
-                        )
-                        ],
-                        md=4,
-                    ),
                     dbc.Col([
                                 dcc.Loading(
                                     id="loading-1",
                                     type="default",
                                     children=dcc.Graph(id='plot-map', config=config)
                                 ),
-                                
-                                dcc.Slider(
-                                    id='map-slider',
-                                    min=0,
-                                    max=len(df.Last_Update.unique())-1,
-                                    value=len(df.Last_Update.unique())-1,
-                                    marks={days: str(pd.to_datetime(df.Last_Update.unique()[days]).strftime("%m/%d")) for days in range(0,len(df.Last_Update.unique()), 7)},
-                                    step=None,
-                                    className="mt-1"
-                                ),
                         ],
-                        md=8,
+                        md=12,
                     )
                 ],
                 justify="center",
             ),
+            #Dropdown
+            dbc.Row(
+                [
+                    dbc.Col([
+                        dcc.Dropdown(
+                            id="selected_country",
+                            options=dropdown_options,
+                            value='Worldwide',
+                            className="mb-2 mt-4",
+                        ),
+                    ],
+                    xl=4,
+                    ),
+                ],
+                justify="center",
+            ),
+            #Cards
+            dbc.Row(
+                [
+                    #Confirmed cases
+                    dbc.Col([
+                        dbc.Card(
+                            dbc.CardBody([
+                                    html.P(["Total Confirmed: ", html.B(id="card-confirmed"),],className="card-text"),         
+                            ]),
+                            inverse=True,
+                            style={"text-align":"center", 'background-color': "#4b6584"}
+                        ),
+                    ],
+                    md=4,
+                    ),
+                    #Recovered cases card-recovered
+                    dbc.Col([  
+                        dbc.Card(
+                            dbc.CardBody([
+                                    html.P(["Total recovered: ", html.B(id="card-recovered"),], className="card-text"),         
+                            ]),
+                            #Total recovered
+                            inverse=True,
+                            style={"text-align":"center", 'background-color': "#778ca3"}
+                        ),
+                    ],
+                    md=4,
+                    ), 
+                    #Deaths cases 
+                    dbc.Col([
+                        dbc.Card(
+                            dbc.CardBody([
+                                    html.P(["Total deaths: ", html.B(id="card-deaths")], className="card-text"),         
+                            ]),
+                            #Total deaths
+                            inverse=True,
+                            style={"text-align":"center", 'background-color': "#a5b1c2"}
+                        ),
+                    ],
+                    md=4,
+                    ),
+                ],
+                justify="center",
+            ),
+            
             dbc.Row(
                 [
                     dbc.Col(
@@ -191,7 +243,7 @@ body_worldwide = dbc.Container(
 @app.callback([Output('title-main', 'children'), Output('card-confirmed', 'children'), Output('card-recovered', 'children'), Output('card-deaths', 'children')],
               [Input('selected_country', 'value')])
 def main_bar(selected_country):
-    main_title="{} evolution".format(selected_country)
+    main_title="Covid19 evolution, {}".format(selected_country)
     
     x_int="{:,}".format(int(df[today & (df["Country_Region"]==selected_country)].Confirmed.sum())).replace(',', ' ')
     y_int="{:,}".format(int(df[today & (df["Country_Region"]==selected_country)].Recovered.sum())).replace(',', ' ')
@@ -200,27 +252,42 @@ def main_bar(selected_country):
 
 
 @app.callback(Output('plot-map', 'figure'),
-              [Input('map-slider', 'value')])
+              [Input('map-slider', 'date')])
 def update_map(map_slider):
         #World Map
-    df_data=df[(df.Last_Update==df.Last_Update.unique()[map_slider])].groupby(['Last_Update', 'Country_Region'], as_index=False)['Confirmed', 'Deaths'].sum()
-        
-    fig = px.scatter_geo(df_data, locations="Country_Region", locationmode = "country names",
-                         color=np.power(df_data["Confirmed"],0.3)-2, size= np.power(df_data["Confirmed"]+1,0.3)-1,
-                         hover_name="Country_Region",
-                         hover_data=["Confirmed"],
-                         animation_frame="Last_Update",
-                         projection="natural earth")
-    fig.update_coloraxes(colorscale="redor")
-    fig.update(layout_coloraxis_showscale=False)
+    df_data=df[(df.Last_Update==map_slider) & (df.Country_Region!="Worldwide")].groupby(['Last_Update', 'Country_Region'], as_index=False)['Confirmed', 'Deaths'].sum()
+    
+    fig=go.Figure(data=go.Choropleth(
+        locations=df_data['Country_Region'],
+        #z=np.power(df_data["Confirmed"],0.3)-2,
+        z=np.log(df_data["Confirmed"]),
+        text=[str(int(elem))+" cases" for elem in df_data["Confirmed"]],
+        locationmode='country names',
+        colorscale=px.colors.diverging.Spectral,
+        autocolorscale=False,
+        marker_line_color='#f2f2f2',
+        colorbar_title="log(Confirmed cases)",
+        reversescale=True,
+        zmin=7.5,
+        zmax=11.8197782,
+    ))
     fig.update_geos(
+        projection_type="natural earth",
+        showframe=False,
+        coastlinecolor="#f2f2f2",
         resolution=110,
         lataxis_showgrid=True, lonaxis_showgrid=True,
-        showland=True, landcolor="palegreen",
-        showocean=True, oceancolor="lightskyblue",
-        showcountries=True, countrycolor="honeydew",
+        
     )
-    fig.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
+    
+    fig.update_layout(
+        height=600,
+        xaxis=menu_ranges,
+        template="plotly_white",
+        margin={"r":0,"l":0,"b":0, "t":0}
+    )
+    
+    
     return fig
 
 
@@ -228,9 +295,9 @@ def update_map(map_slider):
               [Input('selected_country', 'value')])
 def update_daily_cases(selected_country):
     dct = {
-        'Confirmed': {'title': 'Daily new cases', 'color': '#384259'},
-        'Recovered': {'title': 'Daily recovered cases', 'color': '#7ac7c4'},
-        'Deaths': {'title': "Daily new deaths", 'color': '#f73859'}
+        'Confirmed': {'title': 'Daily new cases', 'color': color_confirmed},
+        'Recovered': {'title': 'Daily recovered cases', 'color': color_recovered},
+        'Deaths': {'title': "Daily new deaths", 'color': color_deaths}
     }
     df_data=df[df["Country_Region"]==selected_country].groupby(["Last_Update"]).sum()
     labels=df_data.index[1:]
@@ -242,7 +309,7 @@ def update_daily_cases(selected_country):
             layout_title_text=dct[col]['title']
         )
         fig.update_traces(marker_color=dct[col]['color'])
-        fig.update_layout(title_text=dct[col]['title'], yaxis=dict(range=[0, yaxis_max]), height=300, margin={"b":0})
+        fig.update_layout(xaxis=menu_ranges, template="plotly_white", title_text=dct[col]['title'], yaxis=dict(range=[0, yaxis_max]), height=300, margin={"b":0})
         return fig
     graphs = {col: bar_graph(values, col) for col in dct.keys()}    
     return graphs['Confirmed'], graphs['Recovered'], graphs['Deaths']
@@ -261,45 +328,29 @@ def infection_cases(selected_country):
     fig = go.Figure()
     fig.add_trace(go.Scatter(
         x=labels[7:], y=values,
-        line=dict(color='red'),
+        line=dict(color=color_confirmed),
         name="Rate"
     ))
     
     fig.add_trace(go.Scatter(
         x=labels, y=[1]*len(labels),
-        line=dict(color='green', dash='dash'),
+        line=dict(color=color_deaths, dash='dash'),
         showlegend=False
     ))
     
-    # Add range slider
     fig.update_layout(
         yaxis=dict(range=[0, 3]),
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                    dict(count=1,
-                         label="MTD",
-                         step="month",
-                         stepmode="todate"),
-                    dict(count=1,
-                         label="1m",
-                         step="month",
-                         stepmode="backward"),
-                    dict(count=3,
-                         label="3m",
-                         step="month",
-                         stepmode="backward"),
-                    dict(count=6,
-                         label="6m",
-                         step="month",
-                         stepmode="backward"),
-                    dict(step="all")
-                ])
-            ),
-            type="date"
-        )
+        xaxis=menu_ranges,
+        legend=dict(
+            x=0,
+            y=1.0,
+            bgcolor='rgba(255, 255, 255, 0.5)',
+            bordercolor='rgba(255, 255, 255, 0)'
+        ),
+        title_text="Average number of people infected by an infected individual",
+        height=300,
+        template="plotly_white"
     )
-    fig.update_layout(title_text="Average number of people infected by an infected individual", height=300, margin={"b":0})
     return fig
 
 
@@ -312,28 +363,26 @@ def update_cumulative_cases(selected_country):
                     x=df[df["Country_Region"]==selected_country].groupby(["Last_Update"]).Confirmed.sum().index,
                     y=df[df["Country_Region"]==selected_country].groupby(["Last_Update"]).Confirmed.sum(),
                     name="Confirmed",
-                    line_color='#384259',
+                    line_color=color_confirmed,
                     opacity=0.8))
     #Recovered
     fig.add_trace(go.Scatter(
                     x=df[df["Country_Region"]==selected_country].groupby(["Last_Update"]).Recovered.sum().index,
                     y=df[df["Country_Region"]==selected_country].groupby(["Last_Update"]).Recovered.sum(),
                     name="Recovered",
-                    line_color='#7ac7c4',
+                    line_color=color_recovered,
                     opacity=0.8))
     #Deaths
     fig.add_trace(go.Scatter(
                     x=df[df["Country_Region"]==selected_country].groupby(["Last_Update"]).Deaths.sum().index,
                     y=df[df["Country_Region"]==selected_country].groupby(["Last_Update"]).Deaths.sum(),
                     name="Deaths",
-                    line_color='#f73859',
+                    line_color=color_deaths,
                     opacity=0.8))
-    
-    # Use date string to set xaxis range
-    fig.update_layout(title_text="Cumulative cases evolution", height=300, margin={"b":0})
     
     # Add Button
     fig.update_layout(
+        xaxis=menu_ranges,
         updatemenus=[
             dict(
                 type = "buttons",
@@ -352,12 +401,25 @@ def update_cumulative_cases(selected_country):
                 ]),
                 pad={"r": 0, "t": 0},
                 showactive=True,
-                x=1.2,
+                x=1,
                 xanchor="right",
                 y=1,
                 yanchor="bottom"
             ),
         ]
+    )
+    
+    fig.update_layout(
+        title_text="Cumulative cases evolution", 
+        height=300, 
+        margin={"b":0}, 
+        legend=dict(
+            x=0,
+            y=1.0,
+            bgcolor='rgba(255, 255, 255, 0.5)',
+            bordercolor='rgba(255, 255, 255, 0)'
+        ),
+        template="plotly_white",
     )
     return fig
 
